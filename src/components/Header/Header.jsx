e48da8d3f19e86ce4894b4790ba9d9e3de829c48
Lucas Gola da Silva <lucasgola@outlook.com>
2024-12-04 11:08:42 -0300

    [add] logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
+import logo from '../../assets/LOGO.png'; // Caminho para a logo
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-left">
-        <h1>Sistema de Irrigação Automatizado</h1>
+        <img src={logo} alt="Logo" className="logo" /> {/* Adicionando a logo */}
+        <h1 className="title">
+          Sistema de Irrigação <span className="eco">Eco-</span><span className="inteligente">Inteligente</span>
+        </h1> {/* Alterando o título */}
       </div>
       <nav className="nav-menu">
         <ul>
